9fac3586a79772763438a99b6025b9f056888770
eslam <eslamhassannew@gmail.com>
2023-06-18 16:07:21 +0300

    User Profile and Posts

--- flasktasks/__init__.py
+++ flasktasks/__init__.py
@@ -15,7 +15,7 @@ db = SQLAlchemy(app)
 
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'login_endpoint'
 
 from flasktasks import routes
 from flasktasks.routes import users
